01b3c50f835a393ddc1b2f06170a5130e4e47008
Jeffrey Monzili <jeffrey.monzili@gmail.com>
2022-07-23 12:22:36 +0200

    MAJ des commenataire des fichers des middleware backend

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,15 +7,19 @@ dotenv.config();
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.JWT_TOKEN); // Méthode verify de jwt permet de vérifier la validité d'un token
+    //  Méthode verify de jwt permet de vérifier la validité d'un token
+    const decodedToken = jwt.verify(token, process.env.JWT_TOKEN);
     const userId = decodedToken.userId;
     req.auth = { userId };
     if (req.body.userId && req.body.userId !== userId) {
+      //  s'arrete si le Id n'est pas connu
       throw 'User ID non valable !';
     } else {
+      //  Sinon poursuit vers la fonction
       next();
     }
   } catch (error) {
+    //  en cas d'erreurs
     res.status(401).json({ error: error | 'Requête non authentifiée !' });
   }
 };
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,31 +1,31 @@
-// Configuration de multer pour les images
-const multer = require("multer"); // Importation de multer
-const fs = require("fs");
+// Configuration de multer pour la gestion des fichiers
+const multer = require('multer');
+const fs = require('fs');
 
 // Dictionnaire d'extensions
 const MIME_TYPES = {
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-  "image/png": "png",
-  "image/gif": "gif",
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png',
+  'image/gif': 'gif',
 };
 
-// Configuration du chemin et du nom du fichier pour les fichiers entrants
+//  Configuration du chemin et du nom du fichier pour les fichiers entrants
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    // Enregistre le fichier dans le dossier images
-    callback(null, "images");
+    //  Enregistre le fichier dans le dossier images
+    callback(null, 'images');
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_");
-    // Génère nom du fichier, élimine les espaces avec split et join
+    const name = file.originalname.split(' ').join('_');
+    //  Génère nom du fichier, élimine les espaces avec split et join
     const extension = MIME_TYPES[file.mimetype]; // Accès mimetype (ex: image / jpg)
-    callback(null, name + "_" + Date.now() + "." + extension); // Génère un nom, un time-stamp et une extension
+    callback(null, name + '_' + Date.now() + '.' + extension); // Génère un nom, un time-stamp et une extension
   },
 });
 
-// Exportation du middleware multer qui prend en paramètre l'objet storage, single permet de renvoyer un fichier image unique
+//  Exportation du middleware multer qui prend en paramètre l'objet storage, single permet de renvoyer un fichier image unique
 module.exports = multer({
   storage: storage,
   limits: { fileSize: 600000 },
-}).single("attachment");
+}).single('attachment');
